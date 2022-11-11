f66a480b80f16351427fdcd9a98eca7e4430cc85
archiveAwesome <117956813+archiveAwesome@users.noreply.github.com>
2022-11-10 22:01:27 -0300

    Add files via upload

---
+++ js/cadastro.js
@@ -0,0 +1,65 @@
+// function cadastrar(){
+//     window.location.href = "api.html"
+// }
+
+
+// function onChangeEmail(){
+//     toggleButtonDisable()
+//     toggleEmailErrors()
+    
+
+// }
+
+// function onChangePassword(){
+//     toggleButtonDisable()
+//     togglePasswordErros()
+    
+
+// }
+
+
+
+
+// function isEmailValid(){
+// const email = form.email().value;
+// const email2 = form.email2().value;
+// if(!email ){
+//   return false;
+// }
+// return true
+
+// }
+
+
+
+
+
+
+// function toggleButtonDisable(){
+//     const emailValid = isEmailValid();
+//     form.savebtn().disabled = !emailValid;        
+
+
+  
+
+// }
+
+
+
+
+
+
+// const form = {
+//     email: () => document.getElementById('email'),
+//     email2: () => document.getElementById('email2'),
+//     savebtn: () => document.getElementById('save-btn'),
+// }
+
+
+
+
+// function cadastrar(){
+//     window.location.href = "api.html"
+// }
+
+
